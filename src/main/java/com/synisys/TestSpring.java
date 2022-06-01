1a7f723c48adf806dc597d6c6d8b73d36baa925a
Vahe <vah.aghajanyan@gmail.com>
2022-06-01 09:54:57 +0400

    1. Setting up configs and creating first bean.

---
+++ src/main/java/com/synisys/TestSpring.java
@@ -0,0 +1,14 @@
+package com.synisys;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+                "applicationContext.xml");
+
+        TestBean testBean = applicationContext.getBean("testBean", TestBean.class);
+        System.out.println(testBean.getName());
+        applicationContext.close();
+    }
+}
