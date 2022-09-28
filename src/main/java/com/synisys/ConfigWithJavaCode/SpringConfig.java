9169c8cfb05ecee17057aae97519292e8d564821
Vahe <vah.aghajanyan@gmail.com>
2022-09-28 08:46:24 +0400

    Configurations with Java code.

--- src/main/java/com/synisys/ConfigWithJavaCode/SpringConfig.java
+++ src/main/java/com/synisys/ConfigWithJavaCode/SpringConfig.java
@@ -1,7 +1,27 @@
 package com.synisys.ConfigWithJavaCode;
 
-import org.springframework.context.annotation.Configuration;
+import com.synisys.ClassicalMusic;
+import com.synisys.MusicPlayer;
+import com.synisys.RockMusic;
+import org.springframework.context.annotation.*;
 
 @Configuration
+@ComponentScan("com.synisys")
+@PropertySource("musicPlayer.properties")
 public class SpringConfig {
+
+    @Bean
+    public ClassicalMusic classicalMusic() {
+        return new ClassicalMusic();
+    }
+    @Bean
+    public RockMusic rockMusic() {
+        return new RockMusic();
+    }
+
+    public MusicPlayer musicPlayer() {
+        return new MusicPlayer(classicalMusic(), rockMusic());
+    }
+
+
 }
--- src/main/java/com/synisys/TestSpring.java
+++ src/main/java/com/synisys/TestSpring.java
@@ -1,11 +1,15 @@
 package com.synisys;
 
+import com.synisys.ConfigWithJavaCode.SpringConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(
-                "applicationContext.xml");
+        /*ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+                "applicationContext.xml");*/
+
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringConfig.class);
 
         MusicPlayer musicPlayer = applicationContext.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic(MusicType.CLASSICAL);
